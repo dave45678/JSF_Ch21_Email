c96929c0eb44777b944b240dba6203eaf2ad05b2
Dave Wolf <dave45678@gmail.com>
2015-09-04 16:54:42 -0400

    first commit
    book example from ch 21 simply works.

---
+++ Ch21_jsf_email/src/MessageBean.java
@@ -0,0 +1,14 @@
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class MessageBean {
+    private String message = "<h1>Hello, world!</h1>";
+    
+    public String getMessage() {
+        return message;
+    }
+    
+    public void setMessage(String message) {
+        this.message = message;
+    }    
+}
